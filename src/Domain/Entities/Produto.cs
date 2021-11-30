ba59fa4ac302389686e96521d294918521976e88
michael-silva-dti <91736102+michael-silva-dti@users.noreply.github.com>
2021-11-30 17:09:37 -0300

    Adicionando classe de produto

---
+++ src/Domain/Entities/Produto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class Produto
+    {
+        public Guid Id { get; set; }
+
+        public string Nome { get; set; }
+
+        public string Descricao { get; set; }
+
+        public double Valor { set; get; }
+
+        public int Quantidade { set; get; }
+    }
+}
