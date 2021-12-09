a9771585c2a4f0c9b203f82d26ccd985cf15907d
michael-silva-dti <91736102+michael-silva-dti@users.noreply.github.com>
2021-12-09 13:54:04 -0300

    Adicionando os testes para o método ValidarCadastro do ClienteService

--- src/UnitTests/Domain/ClienteServiceTest.cs
+++ src/UnitTests/Domain/ClienteServiceTest.cs
@@ -15,28 +15,77 @@ namespace UnitTests.Domain
     public class ClienteServiceTest
     {
         [Fact]
-        public async void ValidarCadastro_DeveRetornarFalse()
+        public async void ValidarCadastro_RetornarFalsoParaAusênciaDeDadosNoRepositorio()
         {
+            // Arrange
             Cliente c = new Cliente();
             var repositoryMock = new Mock<IClienteRepository>();
             var service = new ClienteService(repositoryMock.Object);
-            //repositoryMock.Setup(p=>p.BuscarTodosOsClientes()).Returns(null);
+
+            // Act
             var Result = await service.ValidarCadastro(c);
-            Assert.False(Result, "A validação está aceitando clientes nulos como entrada.");
+
+            //Assert
+            Assert.False(Result, "Entradas nulas não estão sendo validadas");
         }
 
         [Fact]
-        public void ValidarCadastro_retornaTrueParaDadosCorretos() 
+        public void ValidarCadastro_RetornaPositivoParaEntradaComMesmoClienteNoBanco()
         {
-            Cliente c = new Cliente() {Nome= "Michael", CPF= "13602151662",Email = "michael@.com" };
+            // Arrange
+            Cliente c = new Cliente() { Nome = "Michael", CPF = "734.408.754-58", Email = "michael@.com" };
             List<Cliente> responseMockList = new List<Cliente>();
-            responseMock.Add(c);
-            IEnumerable<Cliente> responseMock = responseMockList as IEnumerable;
+            responseMockList.Add(c);
+            IEnumerable<Cliente> responseMock = responseMockList as IEnumerable<Cliente>;
+            var repositoryMock = new Mock<IClienteRepository>();
+            repositoryMock.Setup(p => p.BuscarTodosOsClientes()).ReturnsAsync(responseMock);
+            ClienteService service = new ClienteService(repositoryMock.Object);
 
-            var repositoryMock = new Mock<ClienteRepository>();
-            repositoryMock.Setup(p=>p.BuscarTodosOsClientes()).Returns(responseMock);
+            // Act
+            bool resultado = service.ValidarCadastro(c).Result;
 
-            var service = new ClienteService(repositoryMock.Object);
+            // Assert
+            Assert.True(resultado, "O retorno está falso mesmo com a entrada com entidade já presente no banco");
         }
+
+        [Fact]
+        public void ValidarCadastro_RetornaTrueCasoExistaEntidadeComMesmoEmailNoBanco()
+        {
+            // Arrange
+            Cliente cliente = new Cliente() {Nome = "Michael", CPF="13602151662", Email="michael@.com" };
+            Cliente clienteErrado = new Cliente() {Nome = "Michael", CPF="13602151663", Email="michael@.com" };
+            IEnumerable<Cliente> clientes = new List<Cliente>() {cliente } as IEnumerable<Cliente>;
+            var repository = new Mock<IClienteRepository>();
+            repository.Setup(p => p.BuscarTodosOsClientes()).ReturnsAsync(clientes);
+            ClienteService service = new ClienteService(repository.Object);
+
+            // Act
+            bool resultado = service.ValidarCadastro(clienteErrado).Result;
+
+            // Assert
+            Assert.True(resultado, "Mesmo com o email igual a outro objeto armazenado o retorno é falso");
+        }
+
+        [Fact]
+        public void ValidarCadastro_RetornaTrueParaObjetoComMesmoCPFExistenteNoBanco()
+        {
+            // Arrange
+            Cliente cliente = new Cliente() { Nome = "Michael", CPF = "13602151662", Email = "michael@.com" };
+            Cliente clienteErrado = new Cliente() { Nome = "Michael", CPF = "13602151662", Email = "Michael@.com" };
+            IEnumerable<Cliente> clientes = new List<Cliente>() { cliente } as IEnumerable<Cliente>;
+            var repository = new Mock<IClienteRepository>();
+            repository.Setup(p => p.BuscarTodosOsClientes()).ReturnsAsync(clientes);
+            ClienteService service = new ClienteService(repository.Object);
+
+            // Act
+            bool resultado = service.ValidarCadastro(clienteErrado).Result;
+
+            // Assert
+            Assert.True(resultado, "Mesmo com cliente com mesmo CPF cadastrado o retorno está falso");
+        }
+
+
+
+
     }
 }
