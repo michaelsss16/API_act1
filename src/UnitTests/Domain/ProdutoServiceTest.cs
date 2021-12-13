9dce3b9e2da3ae8054ea4e865f222c17cf0f75a1
michael-silva-dti <91736102+michael-silva-dti@users.noreply.github.com>
2021-12-13 10:02:30 -0300

    Adicionando classe de testes do serviço de produto

---
+++ src/UnitTests/Domain/ProdutoServiceTest.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using Moq;
+using Domain.Entities;
+using Domain.Services;
+using Domain.Interfaces.Repositories;
+using Domain.Interfaces.Services;
+using Infrastructure.Repositories;
+
+namespace UnitTests.Domain
+{
+    public class ProdutoServiceTest
+    {
+        [Fact]
+        public void BuscarTodosOsProdutos_TesteDeRetornoNulo()
+        {
+            var repository = new Mock<IProdutoRepository>();
+            ProdutoService service = new ProdutoService(repository.Object);
+            var Resultado = service.BuscarTodosOsProdutos().Result;
+            Assert.Equal(Resultado, new List<Produto>());
+        }
+        [Fact]
+        public void BuscarTodosOsProdutos_TesteDeRetornoParaProdutoComCamposNulos()
+        {
+            Produto produtoNulo = new Produto();
+            IEnumerable<Produto> Lista = new List<Produto>() { produtoNulo } as IEnumerable<Produto>;
+            var repository = new Mock<IProdutoRepository>();
+            repository.Setup(p => p.Get()).ReturnsAsync(Lista);
+            ProdutoService service = new ProdutoService(repository.Object);
+            var Resultado = service.BuscarTodosOsProdutos().Result;
+            Assert.Equal(Resultado, Lista);
+        }
+
+        [Fact]
+        public void BuscarTodosOsProdutos_TesteDeRetornoParaProdutoComCamposPreenchidos()
+        {
+            Produto produto = new Produto() { Id = Guid.NewGuid(), Nome = "ProdutoTeste", Quantidade = 2, Valor = 12345, Descricao = "Produto de teste" };
+            IEnumerable<Produto> Lista = new List<Produto>() { produto } as IEnumerable<Produto>;
+            var repository = new Mock<IProdutoRepository>();
+            repository.Setup(p => p.Get()).ReturnsAsync(Lista);
+            ProdutoService service = new ProdutoService(repository.Object);
+            var Resultado = service.BuscarTodosOsProdutos().Result;
+            Assert.Equal(Resultado, Lista);
+        }
+
+    } // Fim da classe
+}
