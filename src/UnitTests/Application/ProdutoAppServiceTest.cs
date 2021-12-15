f43e9a8722b2deea8079f8a0802c5eb026ea84dd
michael-silva-dti <91736102+michael-silva-dti@users.noreply.github.com>
2021-12-15 14:12:14 -0300

    Adicionando testes unitários para appService de produto

--- src/UnitTests/Application/ProdutoAppServiceTest.cs
+++ src/UnitTests/Application/ProdutoAppServiceTest.cs
@@ -15,8 +15,40 @@ namespace UnitTests.Application
     public class ProdutoAppServiceTest
     {
         [Fact]
-        public void teste()
-        { 
+        public void BusarTodosOsProdutos_RetornaListaDeProdutos()
+        {
+            var lista = new List<Produto>() as IEnumerable<Produto>;
+            var service = new Mock<IProdutoService>();
+            service.Setup(p => p.BuscarTodosOsProdutos()).ReturnsAsync(lista);
+            var appService = new ProdutoAppService(service.Object);
+            var resultado = appService.BuscarTodosOsProdutos().Result;
+            Assert.NotNull(resultado);
+            Assert.Equal(lista, resultado);
         }
+
+        [Fact]
+        public void BuscarProdutoPorId_RetornaProdutoDeAcordoComOId()
+        {
+            var id = Guid.NewGuid();
+            Produto produto = new Produto() { Id = id };
+            var service = new Mock<IProdutoService>();
+            service.Setup(p => p.BuscarProdutoPorId(id)).ReturnsAsync(produto);
+            var appService = new ProdutoAppService(service.Object);
+            var resultado = appService.BuscarProdutoPorId(id).Result;
+            Assert.NotNull(resultado);
+            Assert.Equal(produto, resultado);
+        }
+
+[Fact]
+    public void AdicionarProduto_RetornaMensagemPositivaParaAdicao()
+    {
+        var produtodto = new ProdutoDTO();
+        var service = new Mock<IProdutoService>();
+        service.Setup(p => p.AdicionarProduto(produtodto)).ReturnsAsync("Produto adicionado com sucesso");
+        var appService = new ProdutoAppService(service.Object);
+        var resultado = appService.AdicionarProduto(produtodto).Result;
+        Assert.NotNull(resultado);
+        Assert.Equal("Produto adicionado com sucesso", resultado);
     }
+}// fim da classe
 }
